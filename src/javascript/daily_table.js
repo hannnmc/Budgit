98f75834ce7941df76611b27bd1a2cc16eb7dd02
Han Chen <hanmikechen@gmail.com>
2022-09-18 20:45:45 -0400

    add input field and button

---
+++ src/javascript/daily_table.js
@@ -0,0 +1,12 @@
+// const insertRow = function insert_Row() {
+//     let row =document.getElementById('daily-table').insertRow(0);
+//     let item = row.insertCell(-1);
+//     let category = row.insertCell(1);
+//     let amount = row.insertCell(2);
+//     item.innerHTML="New Cell1";
+//     category.innerHTML="New Cell2";
+//     amount.innerHTML="New Cell2";
+//     amount.classList.add("amount-col");
+//   }
+
+// export default insertRow();
