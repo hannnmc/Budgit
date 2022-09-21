1bcf4e3f6cf9c064ab86dd294cd62c4b0feea2d8
Han Chen <hanmikechen@gmail.com>
2022-09-21 15:14:04 -0400

    add info modal including instructions and future features

--- src/javascript/help_modal.js
+++ src/javascript/help_modal.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", e => {
 
     const openmodal = document.getElementById('help-info');
-    const modal= document.getElementsByClassName('modal-wrapper');
-    const closemodal = document.getElementsByClassName('close-button');
+    const modal= document.querySelector('.modal-wrapper');
+    const closemodal = document.querySelector('.close-button');
 
     openmodal.addEventListener ('click', () => {
         modal.classList.add('active');
     });
-    console.log(closemodal);
     closemodal.addEventListener ('click', () => {
         modal.classList.remove('active');
     });
